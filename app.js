9fc2e08ab965f5ef1afebc1d87af1a6cac2e3ff7
I-love-cats-and-baking <sophie.shufei.gu@gmail.com>
2018-08-01 16:27:13 -0700

    Fixed dropbar, decorated homepage

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log('app.js sanity check');
+
+
+$(document).ready(function(){
+
+  $('h1').click(function(){
+    $(this).css('background-color', '#d8e2f3')
+});
+
+
+
+});//closes document.ready
